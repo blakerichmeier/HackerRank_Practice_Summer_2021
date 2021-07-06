27d1ab3ca135f15da7fdf966942d635e63c01ea6
Blake Richmeier <65925816+blakerichmeier@users.noreply.github.com>
2021-07-06 15:45:48 -0500

    Create practice.cpp
    n/a

---
+++ practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+//2) 
+
+// 1) write the hello world function in c++!!!!
+void hello_world()
+{
+    std::cout << "Hello World!\nGoodbye Moon\n";
+}
+
+int main()
+{
+    hello_world();
+}
