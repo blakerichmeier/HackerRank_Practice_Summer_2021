7b242c0291673b6b054e13189e3b047aa4c4d877
Blake Richmeier <65925816+blakerichmeier@users.noreply.github.com>
2021-07-06 16:12:06 -0500

    traverse valleys

--- practice.cpp
+++ practice.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include <string.h>
+#include <math.h>
 
-//2) 
+//2) return the number of valleys traversed 
+//example string [DDUUDD] 
+int traverse(std::string s)
+{
+    int y = 0;
+    int valleycount = 0;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(y == 0)
+        {
+            valleycount++;
+        }
+        if(s.at(i) == 'D')
+        {
+            y--;
+        }
+        else if(s.at(i) == 'U')
+        {
+            y++;
+        }
+    }
+
+    return(valleycount/2);
+}
 
 // 1) write the hello world function in c++!!!!
 void hello_world()
@@ -11,5 +36,8 @@ void hello_world()
 
 int main()
 {
-    hello_world();
+    std::string myString = "UDDDUDUUU";
+    std::cout << traverse(myString) << "\n";
+
+    return(0);
 }
